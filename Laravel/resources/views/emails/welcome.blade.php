55c151a15694f36347e82e361653764687cc5569
noormesslmani <noor.messlmani@gmail.com>
2023-03-18 21:48:16 +0200

    Creating a welcome email template

---
+++ Laravel/resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Inventory Website!</title>
+</head>
+<body>
+    <h1>Welcome, {{ $name }}!</h1>
+    <p>Thank you for registering at Inventory Website. We're glad to have you as part of our community.</p>
+    <p>If you have any questions or concerns, please don't hesitate to contact us at support@Inventory.com .</p>
+    <p>Thanks again, and enjoy using Inventory Website!</p>
+</body>
+</html>
