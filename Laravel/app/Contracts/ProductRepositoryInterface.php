e95827eafc9114a1ee22960e71a2a13fc8bab104
noormesslmani <noor.messlmani@gmail.com>
2023-03-18 19:20:00 +0200

    Creating ProductRepositoryInterface and  ProductRepository implementation

---
+++ Laravel/app/Contracts/ProductRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\Product;
+
+interface ProductRepositoryInterface
+{
+    public function index(): object;
+    public function search(string $type): object;
+    public function show(int $id): ?Product;
+    public function create(array $data): Product;
+    public function update(array $data, Product $product): Product;
+    public function destroy(Product $product): void;
+}
---
+++ Laravel/app/Repositories/ProductRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Product;
+use App\Models\User;
+use App\Exceptions\NotFoundException;
+use App\Exceptions\ActionForbiddenException;
+use App\Contracts\ProductRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function index(): object{
+        $products= Auth::user()
+        ->products()
+        ->withCount('unsoldItems')
+        ->latest()
+        ->paginate(10);
+        return $products;
+    }
+  
+    public function search(string $type): object{
+        $products=Auth::user()
+        ->products()
+        ->withCount('unsoldItems')
+        ->where('type', 'LIKE', '%'.$type.'%')
+        ->latest()
+        ->paginate(10);
+        return $products;
+    }
+
+    public function create(array $data): Product{
+        return Product::create(array_merge($data,['owner_id'=>Auth::id()]));
+    }
+
+
+    public function show(int $id): ?Product{
+        $product=Product::find($id);
+        return $product;
+    }
+
+    public function update(array $data, Product $product): Product{
+        $product->update($data);
+        return $product;
+
+    }
+    
+    public function destroy(Product $product): void{
+        $product->delete();
+        return;
+    }
+
+}
