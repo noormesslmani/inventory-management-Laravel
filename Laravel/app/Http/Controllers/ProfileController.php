c50a5c53d521d0609d7b54309a8ce6dd7a0dc19c
noormesslmani <noor.messlmani@gmail.com>
2023-03-18 19:19:27 +0200

    Creating ProfileController with index, update, and changePassword methods

---
+++ Laravel/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\ProfileServiceInterface;
+
+use App\Http\Requests\UpdateProfileRequest;
+use App\Http\Requests\ChangePasswordRequest;
+
+use App\Traits\PrepareResponseTrait;
+
+use JWTAuth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
+
+
+class UserController extends Controller
+{
+
+    use PrepareResponseTrait;
+
+    public function __construct(ProfileServiceInterface $profileService)
+    {
+        $this->profileService = $profileService;
+    }
+
+    public function index(): JsonResponse
+    {
+        $user = Auth::user();
+        
+        return $this->prepareResponse(["user"=>$user], 'User retrieved successfully', 200);
+    }
+    
+    public function update(UpdateProfileRequest $request): JsonResponse{
+        
+        $result = $this->profileService->updateProfile($request->validated()); 
+        
+        return $this->prepareResponse($result['data'], $result['message'], $result['http_code']);
+
+    }   
+
+
+    public function changePassword(ChangePasswordRequest $request): JsonResponse
+    {
+        
+        $result = $this->profileService->changePassword($request->validated()); 
+        
+        return $this->prepareResponse($result['data'], $result['message'], $result['http_code']);
+
+       
+    }   
+
+
+}
